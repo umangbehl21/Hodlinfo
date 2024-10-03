770505bfe0de7d41bc19a9f7a939d789bca4afad
Umang Behl <98390701+umangbehl21@users.noreply.github.com>
2024-10-04 02:26:39 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const app = express();
-const port = 3000; // You can choose any port you like
-const {createDatabase,connectToDatabase,pool} = require('./dbConnection')  //https://api.wazirx.com/api/v2/tickers
+const port = 3000; 
+const {createDatabase,connectToDatabase,createTable,pool} = require('./dbConnection')  
 const axios = require('axios')
-// Middleware to parse JSON
 app.use(express.json());
 
-// createDatabase()
- connectToDatabase()
+createDatabase()
+connectToDatabase()
+createTable()
 
 app.post('/api/add/data', async (req, res) => {
     try {
-        // Fetch data from WazirX API
+        
         const response = await axios.get('https://api.wazirx.com/api/v2/tickers');
         const data = response.data;
 
-        // Extract the top 10 results
+        
         const top10Data = Object.keys(data).slice(0, 10).map(key => ({
             name: data[key].name,
             last: data[key].last,
@@ -25,7 +25,6 @@ app.post('/api/add/data', async (req, res) => {
             base_unit: data[key].base_unit
         }));
 
-        // Insert top 10 data into the PostgreSQL database
         for (const item of top10Data) {
             await pool.query(
                 'INSERT INTO hodlinfo (name, last, buy, sell, volume, base_unit) VALUES ($1, $2, $3, $4, $5, $6)',
@@ -50,12 +49,11 @@ app.get('/api/top/results', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-// Basic route to check if server is running
+
 app.get('/', (req, res) => {
     res.send('Hodlinfo API is running!');
 });
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
